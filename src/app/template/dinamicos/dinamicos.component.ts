23eb068b2044fc04b1c7fa5c248f8432d3cddf49
Ildefonso Valenzuela <ponchovalo@outlook.com>
2022-04-09 22:03:34 -0500

    SecondCommit

---
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styles: [
+  ]
+})
+export class DinamicosComponent{
+
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'Ildefonso',
+    favoritos: [
+      {id: 1, nombre: 'Metal Gear'},
+      {id: 2, nombre: 'DeathStranding'}
+    ]
+  }
+
+  guardar(){
+    console.log('Datos Guardados')
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego(){
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+}
